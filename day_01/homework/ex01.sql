a79b91f4feb4ad7e3e9a4851675009f7284f877a
DoDat-12 <dubstep.0509dtd@gmail.com>
2024-11-18 00:25:13 +0700

    homework1&2

---
+++ day_01/homework/ex01.sql
@@ -0,0 +1,33 @@
+SELECT *
+FROM actor_films;
+
+--1 - DDL for actors table
+--struct for film's information
+CREATE TYPE film_stats AS
+(
+    film   TEXT,
+    votes  INTEGER,
+    rating REAL,
+    filmid TEXT
+);
+
+--enum for quality_class
+CREATE TYPE quality_class AS ENUM (
+    'star',
+    'good',
+    'average',
+    'bad'
+    );
+
+--table
+CREATE TABLE actors
+(
+    actorid       TEXT,
+    actor         TEXT,
+    current_year  INTEGER,
+    is_active     BOOLEAN,
+    quality_class quality_class,
+    films         film_stats[],
+    PRIMARY KEY (actorid, current_year)
+);
+-- DROP TABLE actors;
---
+++ day_01/homework/ex02.sql
@@ -0,0 +1,41 @@
+--2 - cumulative table generation query
+--min1970 max2021
+SELECT MIN(year), MAX(year)
+FROM actor_films;
+
+--query
+WITH yesterday AS (SELECT * FROM actors WHERE current_year = 1969),
+     today AS (SELECT actorid,
+                      actor,
+                      year,
+                      AVG(rating)                                              AS rating,
+                      ARRAY_AGG(ROW (film, votes, rating, filmid)::film_stats) AS films
+               FROM actor_films
+               WHERE year = 1970
+               GROUP BY actorid, actor, year)
+SELECT COALESCE(t.actorid, y.actorid)       AS actorid,
+       COALESCE(t.actor, y.actor)           AS actor,
+       COALESCE(t.year, y.current_year + 1) AS current_year,
+       CASE
+           WHEN t.year IS NOT NULL THEN TRUE
+           ELSE FALSE
+           END                              AS is_active,
+       CASE
+           WHEN t.year IS NOT NULL THEN
+               CASE
+                   WHEN t.rating > 8 THEN 'star'
+                   WHEN t.rating > 7 THEN 'good'
+                   WHEN t.rating > 6 THEN 'average'
+                   ELSE 'bad'
+                   END::quality_class
+           ELSE y.quality_class
+           END                              AS quality_class,
+       CASE
+           WHEN y.films IS NULL
+               THEN t.films
+           WHEN t.year IS NOT NULL
+               THEN y.films || t.films
+           ELSE y.films
+           END                              AS films
+FROM today t
+         FULL OUTER JOIN yesterday y ON t.actorid = y.actorid;
